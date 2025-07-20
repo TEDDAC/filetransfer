c2afd9c920ab6f1f5183caba889f674859d53ed2
TEDDAC <mathisribemont@gmail.com>
2025-07-20 20:05:27 +0200

    files sent and redirect

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 const storage = multer.diskStorage({
   destination: 'uploads/',
   filename: function (req, file, callback){
-    const uniquePreffix = `${Date.now()}${Math.round(Math.random() * 1E9)}`
+    const uniquePreffix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`
     callback(null, uniquePreffix + '-' + file.originalname)
   }
 })
@@ -17,10 +17,10 @@ app.use(express.static('public'))
 app.use(express.json())
 
 app.post('/upload', upload.single('file'), function (req, res) {
-  // req.file is the name of your file in the form above, here 'uploaded_file'
-  // req.body will hold the text fields, if there were any
-  console.log(req.file)
-  res.send({status: 200})
+  //console.log(req.file)
+  const body = {id: req.file.filename.split('-')[0]}
+  //console.log(body)
+  res.status(200).send(body)
 })
 
 app.listen(PORT, () => console.log(`Server listening ${PORT}`))
--- public/js/sendFile.js
+++ public/js/sendFile.js
@@ -1,15 +1,16 @@
 const form = document.querySelector("#fileSenderForm")
 const fileField = form.querySelector("#fileField")
 const errorMessage = document.querySelector("#errorMessage")
-
+const responseLinkParagraph = document.querySelector("#responseLink")
+const MAX_SIZE = 2**32 // 4Go
 
 fileField.addEventListener("change", (e) => {
   e.preventDefault()
   
   const file = fileField.files[0]
 
-  if(file.size > 65536){ // 2^26
-    onError('File is too large !!! Max 64Mo')
+  if(file.size > MAX_SIZE){ // 2^26
+    onError('File is too large !!! Max 4Go')
   }
 })
 
@@ -19,8 +20,8 @@ form.addEventListener("submit", async (e) => {
 
   const file = fileField.files[0]
 
-  if(file.size > 65536){ // 2^26
-    onError('File is too large !!! Max 64Mo')
+  if(file.size > MAX_SIZE){ // 2^26
+    onError('File is too large !!! Max 4Go')
   }
 
   const formData = new FormData()
@@ -29,8 +30,11 @@ form.addEventListener("submit", async (e) => {
   const response = fetch("/upload", {
     method: "POST",
     body: formData
-  }).then((res) => {
-    console.debug(res)
+  }).then(async (res) => {
+    const body = await res.json()
+    const id = body.id;
+    const url = `${document.URL}/downloadPage/${id}`
+    window.location.href = url
   }).catch((reason) => {
     console.error(`Request aborted: ${reason}`)
   })
