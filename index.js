be5d3245d771801509062a48c1e7dd5487480bef
TEDDAC <mathisribemont@gmail.com>
2025-07-20 20:31:07 +0200

    schedule job to delete file when too older

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const multer = require('multer')
+const schedule = require('node-schedule')
+const fs = require('fs')
 
+/**
+ * Remove every files older than 7 days.
+ * Run each hour
+ */
+const job = schedule.scheduleJob('0 * * * *', () => {
+  console.log('Cleaning uploads...')
+
+  fs.readdir('uploads/', (err, files) => {
+    if (err) return console.error('Error reading uploads directory:', err);
+    const now = Date.now();
+    files.forEach(file => {
+      const filePath = `uploads/${file}`;
+      fs.stat(filePath, (err, stats) => {
+        if (err) return console.error('Error stating file:', err);
+        if (now - stats.mtimeMs > 7 * 24 * 60 * 60 * 1000) {
+          fs.unlink(filePath, err => {
+            if (err) console.error('Error deleting file:', err);
+            else console.log(`Deleted: ${filePath}`);
+          });
+        }
+      });
+    });
+  });
+})
+
+/**
+ * Describe where and hour file are stored
+ */
 const storage = multer.diskStorage({
   destination: 'uploads/',
   filename: function (req, file, callback){
@@ -20,11 +50,21 @@ app.post('/upload', upload.single('file'), function (req, res) {
   res.status(200).send(body)
 })
 
+/**
+ * Route to access the download page
+ */
 app.get('/downloadPage/:id', (req, res) => {
-  console.log(`Paramètre ${JSON.stringify(req.params)}`)
+  //console.log(`Paramètre ${JSON.stringify(req.params)}`)
   res.status(200).send('Page en cours de maintenance')
 })
 
+/**
+ * Route that deliver the file
+ */
+app.get('/download/:id', (req, res) => {
+  //console.log(`Paramètre ${JSON.stringify(req.params)}`)
+  res.status(204).send('Page en cours de maintenance')
+})
 
 app.use(express.static('public'))
 
