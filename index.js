1b090471242398397b22876bd5e49e67138ec3e4
TEDDAC <mathisribemont@gmail.com>
2025-07-20 17:34:55 +0200

    file correctly downloaded

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, callback){
+    const uniquePreffix = `${Date.now()}${Math.round(Math.random() * 1E9)}`
+    callback(null, uniquePreffix + '-' + file.originalname)
+  }
+})
+const upload = multer({storage: storage})
 const PORT = 80
 
 const app = express()
@@ -11,7 +19,7 @@ app.use(express.json())
 app.post('/upload', upload.single('file'), function (req, res) {
   // req.file is the name of your file in the form above, here 'uploaded_file'
   // req.body will hold the text fields, if there were any
-  console.log(req.file, req.body)
+  console.log(req.file)
   res.send({status: 200})
 })
 
